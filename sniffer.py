f9c8c57d235189569ad72212f6281312f45b9080
e10withadot <e10withadot@gmail.com>
2025-02-26 11:35:19 +0200

    formatted to .csv instead of .md

--- sniffer.py
+++ sniffer.py
@@ -1,65 +1,121 @@
 '''
 Packet sniffer. Sniffs packets to get IP, TCP, and TLS headers, packet inter-arrivals, flow size, and flow volume.
 '''
+import csv
 import scapy.all as scapy
 from scapy.layers.tls.all import TLS
 from scapy.layers.inet import IP, TCP
 from scapy.sessions import TCPSession
 
-def write_to_file(self, packet: scapy.Packet):
+def file_is_empty(filename: str) -> bool:
     '''
-    Writes packet data to .md file.
+    Returns True if file is empty or doesn't exist, false otherwise.
     '''
-    if packet.haslayer(TCP) and packet.haslayer(TLS):
-        # Packet data
-        pkt_head = f'## Packet {self.flow_size+1}'
-        ip_header = f'''
-### IP Header
-**Version:** {packet[IP].version}\n
-**Internet Header Length:** {packet[IP].ihl}\n
-**Type of Service:** {packet[IP].tos}\n
-**Total Length:** {packet[IP].len}\n
-**Identification:** {packet[IP].id}\n
-**IP Flags:** {packet[IP].flags}\n
-**Fragment Offset:** {packet[IP].frag}\n
-**Time to Live(TTL):** {packet[IP].ttl}\n
-**Protocol:** {packet[IP].proto}\n
-**Header Checksum:** {packet[IP].chksum}\n
-**Source Address:** {packet[IP].src}\n
-**Destination Address:** {packet[IP].dst}\n
-**IP Options:** {packet[IP].options}\n'''
-        tcp_header = f'''
-### TCP Header
-**Source Port:** {packet[TCP].sport}\n
-**Destination Port:** {packet[TCP].dport}\n
-**Sequence:** {packet[TCP].seq}\n
-**ACK:** {packet[TCP].ack}\n
-**TCP Header Length:** {packet[TCP].dataofs}\n
-**Reserved:** {packet[TCP].reserved}\n
-**TCP Flags:** {packet[TCP].flags}\n
-**TCP Window:** {packet[TCP].window}\n
-**Header Checksum:** {packet[TCP].chksum}\n
-**Urgent Pointer:** {packet[TCP].urgptr}\n
-**TCP Options:** {packet[TCP].options}\n'''
-        tls_header = f'''
-### TLS Header
-**Content Type:** {packet[TLS].type}\n
-**Version:** {packet[TLS].version}\n
-**TLS Header Length:** {packet[TLS].len}\n
-**IV:** {packet[TLS].iv}\n
-**MAC:** {packet[TLS].mac}\n
-**Padding:** {packet[TLS].pad}\n
-**Padding Length:** {packet[TLS].padlen}\n'''
-# **Message List:** {packet[TLS].msg}
-        misc = f'''
-### Miscellanous Data
-**Packet Size:** {len(packet)}\n'''
-        # write to file
-        with open('output.md', 'a') as output:
-            output.write(pkt_head+ip_header+tcp_header+tls_header+misc)
+    try:
+        with open(filename, 'r') as file:
+            first_char = file.read(1)
+            if not first_char:
+                return True
+        return False
+    except FileNotFoundError:
+        return True
 
-try:
-    scapy.sniff(session= TCPSession, iface= "eth0", prn= lambda pkt: write_to_file(pkt), store= 0)
-except KeyboardInterrupt:
-    # finish
-    print("Done.")
+class PacketSniffer:
+    def __init__(self):
+        self.packet_no: int = 0
+    
+    def write_to_file(self, packet: scapy.Packet):
+        '''
+        Writes packet data to a .csv file.
+        '''
+        if packet.haslayer(TCP) and packet.haslayer(TLS):
+            # write to file
+            is_empty = file_is_empty('packets.csv')
+            with open('packets.csv', 'a') as packets:
+                writer= csv.writer(packets, delimiter=',')
+                # write first row if needed
+                if is_empty:
+                    writer.writerow([
+                        "Packet No.",
+                        "Packet Size",
+                        "IP Version",
+                        "Internet Header Length(IHL)",
+                        "Type of Service",
+                        "IP Packet Length",
+                        "Identification",
+                        "IP Flags",
+                        "Fragment Offset",
+                        "Time to Live(TTL)",
+                        "Protocol",
+                        "IP Checksum",
+                        "Source Address",
+                        "Destination Address",
+                        "IP Options",
+                        "Source Port",
+                        "Destination Port",
+                        "Seq",
+                        "ACK",
+                        "TCP Header Length",
+                        "Reserved",
+                        "TCP Flags",
+                        "TCP Window",
+                        "TCP Checksum",
+                        "Urgent Pointer",
+                        "TCP Options",
+                        "Content Type",
+                        "TLS Version",
+                        "TLS Header Length",
+                        "IV",
+                        "MAC",
+                        "TLS Padding",
+                        "TLS Padding Length"
+                    ])
+                # row for current packet
+                writer.writerow([
+                    self.packet_no,
+                    len(packet),
+                    packet[IP].version,
+                    packet[IP].ihl,
+                    packet[IP].tos,
+                    packet[IP].len,
+                    packet[IP].id,
+                    packet[IP].flags,
+                    packet[IP].frag,
+                    packet[IP].ttl,
+                    packet[IP].proto,
+                    packet[IP].chksum,
+                    packet[IP].src,
+                    packet[IP].dst,
+                    packet[IP].options,
+                    packet[TCP].sport,
+                    packet[TCP].dport,
+                    packet[TCP].seq,
+                    packet[TCP].ack,
+                    packet[TCP].dataofs,
+                    packet[TCP].reserved,
+                    packet[TCP].flags,
+                    packet[TCP].window,
+                    packet[TCP].chksum,
+                    packet[TCP].urgptr,
+                    packet[TCP].options,
+                    packet[TLS].type,
+                    packet[TLS].version,
+                    packet[TLS].len,
+                    packet[TLS].iv,
+                    packet[TLS].mac,
+                    packet[TLS].pad,
+                    packet[TLS].padlen
+                ])
+            # increase packet no
+            self.packet_no+=1
+
+    def run(self):
+        try:
+            scapy.sniff(session= TCPSession, iface= "eth0", prn= lambda pkt: self.write_to_file(pkt), store= 0)
+        except KeyboardInterrupt:
+            # finish
+            print("Done.")
+
+sniffer = PacketSniffer()
+print("Started packet sniffing. Ctrl + C to quit.")
+sniffer.run()
