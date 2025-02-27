5497a2a41a28f454a42e88561747637e2b8ec06f
e10withadot <e10withadot@gmail.com>
2025-02-27 11:13:31 +0200

    added comments and docs

--- plotter.py
+++ plotter.py
@@ -1,15 +1,19 @@
+'''
+This script generates plots based on the fields in the packets.csv file, compared to the amount of packets in that field.
+You can add or remove fields in the array below to be able to generate more kinds of plots.
+'''
 from collections import defaultdict
 import csv
 import matplotlib.pyplot as plot
 
-# ADD FIELDS HERE
+# ADJUST FIELDS HERE
 fields = [
     "Packet Size",
     "Time to Live(TTL)",
     "TCP Window",
     "TLS Version"
 ]
-
+# map of fields and their amounts
 field_map = defaultdict(dict[str, int])
 
 def dict_add(field: str, map: dict) -> tuple:
@@ -32,6 +36,7 @@ try:
     # graph display interface
     options = '\n'.join([f'\t({i}) {field}/Amount' for i, field in enumerate(fields)])
     c = int(input(f'What would you like to see? (Ctrl + C to quit)\n{options}\n'))
+    # get plot values
     xlabel= fields[c]
     map = field_map[xlabel]
     # generate plot
@@ -41,4 +46,4 @@ try:
     plot.ylabel('Amount')
     plot.show()
 except KeyboardInterrupt:
-    print('Operation Cancelled.\n')
+    print('\nOperation Cancelled.')
