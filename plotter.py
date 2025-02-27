a17c7390e31799677011112fe80dee4c7455eae1
e10withadot <e10withadot@gmail.com>
2025-02-27 11:07:42 +0200

    plotter now adjusts based on fields array

--- plotter.py
+++ plotter.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import csv
 import matplotlib.pyplot as plot
 
@@ -5,6 +6,16 @@ pkt_sizes: dict[str, int] = {}
 ttls: dict[str, int] = {}
 win_sizes: dict[str, int] = {}
 tls_vers: dict[str, int] = {}
+
+# ADD FIELDS HERE
+fields = [
+    "Packet Size",
+    "Time to Live(TTL)",
+    "TCP Window",
+    "TLS Version"
+]
+
+field_map = defaultdict(dict[str, int])
 
 def dict_add(field: str, map: dict) -> tuple:
     '''
@@ -16,43 +27,19 @@ def dict_add(field: str, map: dict) -> tuple:
         map[field]= 1
     return map
 
-with open('packets.csv', 'r') as packetfile:
-    reader= csv.DictReader(packetfile)
+with open('packets.csv', 'r') as packets:
+    reader= csv.DictReader(packets)
     # get fields from rows
     for row in reader:
-        fields = [
-            (row['Packet Size'], pkt_sizes),
-            (row['Time to Live(TTL)'], ttls),
-            (row['TCP Window'], win_sizes),
-            (row['TLS Version'], tls_vers)
-        ]
-        for field, map in fields:
-            map = dict_add(field, map)
-
-# graph display interface
-c = int(input('''What would you like to see? (Ctrl + C to quit)
-      (1) Packet Size/Amount
-      (2) Time to Live/Amount
-      (3) TCP Window/Amount
-      (4) TLS Version/Amount\n'''))
+        for field in fields:
+            field_map[field] = dict_add(row[field], field_map[field])
 try:
-    map: dict = {}
-    xlabel: str = ''
-    if c == 1:
-        map= pkt_sizes
-        xlabel= 'Packet Size'
-    elif c == 2:
-        map= ttls
-        xlabel= 'Time to Live'
-    elif c == 3:
-        map= win_sizes
-        xlabel= 'Window Size'
-    elif c == 4:
-        map= tls_vers
-        xlabel= 'TLS Version'
-    else:
-        raise KeyError('Invalid value. Pick a value between 1 and 4.')
-
+    # graph display interface
+    options = '\n'.join([f'\t({i}) {field}/Amount' for i, field in enumerate(fields)])
+    c = int(input(f'What would you like to see? (Ctrl + C to quit)\n{options}\n'))
+    xlabel= fields[c]
+    map = field_map[xlabel]
+    # generate plot
     plot.bar(map.keys(), map.values())
     plot.title(f'{xlabel} in relation to amount')
     plot.xlabel(xlabel)
